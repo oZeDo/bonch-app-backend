5b81edb006082cafe25599595c97882bcc1915d0
oZeDo <ooooZeDoooo@gmail.com>
2021-09-10 04:14:32 +0300

    feat: new relations in timetable models (Faculty, Group, Tutor, Timetable)

--- main/timetable/models.py
+++ main/timetable/models.py
@@ -3,25 +3,35 @@ from core.models import User
 
 
 # Create your models here.
+class Faculty(models.Model):
+    name = models.CharField(max_length=128)
+
+    def __str__(self):
+        return self.name
+
+
 class Group(models.Model):
-    group = models.CharField(max_length=20)
-    faculty = models.CharField(max_length=64)
+    name = models.CharField(max_length=16)
+    faculty = models.ForeignKey(Faculty, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
 
 
 class Tutor(models.Model):
-    short = models.CharField(max_length=100)
+    short = models.CharField(max_length=128)
     long = models.CharField(max_length=128)
 
 
 class Timetable(models.Model):
+    # probably !TODO: move time to date. Make start_time and end_time as DateTime
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     date = models.DateTimeField()
-    faculty = models.CharField(max_length=64)
-    group = models.CharField(max_length=20)
-    pair = models.CharField(max_length=5)
+    faculty = models.ForeignKey(Faculty, on_delete=models.CASCADE)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+    pair = models.PositiveIntegerField()
     place = models.CharField(max_length=64, null=True)
     subject = models.CharField(max_length=256)
     subject_type = models.CharField(max_length=48)
-    time = models.CharField(max_length=20)
-    tutor = models.CharField(max_length=100, null=True)
-    tutor_full = models.CharField(max_length=256, null=True)
+    time = models.CharField(max_length=32)  # "10:45-12:20"
+    tutor = models.ForeignKey(Tutor, on_delete=models.CASCADE)
