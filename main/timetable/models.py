4936d73ede7d1722d6626d976a75f791a5a798bc
oZeDo <ooooZeDoooo@gmail.com>
2021-09-14 03:17:32 +0300

    feat: deleted Tutor model, moved tutor to Timetable model, added uniqueness for timetable.

--- main/timetable/models.py
+++ main/timetable/models.py
@@ -4,27 +4,23 @@ from core.models import User
 
 # Create your models here.
 class Faculty(models.Model):
-    name = models.CharField(max_length=128)
+    name = models.CharField(max_length=128, unique=True)
 
     def __str__(self):
         return self.name
 
 
 class Group(models.Model):
-    name = models.CharField(max_length=16)
+    name = models.CharField(max_length=16, unique=True)
     faculty = models.ForeignKey(Faculty, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
 
 
-class Tutor(models.Model):
-    short = models.CharField(max_length=128)
-    long = models.CharField(max_length=128)
-
-
 class Timetable(models.Model):
     # probably !TODO: move time to date. Make start_time and end_time as DateTime
+    # probably !TODO: remove pair number
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     date = models.DateTimeField()
     faculty = models.ForeignKey(Faculty, on_delete=models.CASCADE)
@@ -34,4 +30,9 @@ class Timetable(models.Model):
     subject = models.CharField(max_length=256)
     subject_type = models.CharField(max_length=48)
     time = models.CharField(max_length=32)  # "10:45-12:20"
-    tutor = models.ForeignKey(Tutor, on_delete=models.CASCADE)
+    tutor = models.CharField(max_length=128, null=True)
+    tutor_fullname = models.CharField(max_length=128, null=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    unique_together = ['date', 'faculty', 'group', 'pair', 'place', 'subject', 'subject_type', 'time',
+                       'tutor', 'tutor_fullname']
